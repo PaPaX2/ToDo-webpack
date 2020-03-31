14532ca8ffa05daaf9eaa12d038edd469674a764
Kajetan Terlecki <k.terlecki@wp.pl>
2020-03-31 16:49:15 +0200

    Done App based on unique id

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import io from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid'
 
 class App extends React.Component {
   
   state = {
     task: '',
-    taskName: [],
+    taskName: 
+      [
+        {id: '', name: ''}
+      ],
   };
   
   componentDidMount() {
@@ -18,7 +22,7 @@ class App extends React.Component {
   render() {
 
     this.removeTask = (id, local) => {
-      this.state.taskName.splice(id, 1);
+      this.state.taskName.splice(this.state.taskName.findIndex(name => name.id === id), 1);
       this.setState({taskName: this.state.taskName});
       if (local === 'local') {
       this.socket.emit('removeTask', id);
@@ -33,9 +37,13 @@ class App extends React.Component {
     }
 
     this.addTask = (newTask) => {
-      this.state.taskName.push(newTask);
+      this.state.taskName.push({
+        id: uuidv4(),
+        name: newTask
+      });
       this.setState({taskName: this.state.taskName})
-      
+      console.log(this.state.taskName);
+
     }
 
     return (
@@ -49,11 +57,11 @@ class App extends React.Component {
           <h2>Tasks</h2>
     
           <ul className="tasks-section__list" id="tasks-list">
-            {this.state.taskName.map((row, index) => (
-              <li key={index} className="task">{row}
+            {this.state.taskName.map((row) => (
+              <li key={row.id} className="task">{row.name}
                 <button 
                 className="btn btn--red"
-                onClick={(source) => this.removeTask(index, source="local")}
+                onClick={(source) => this.removeTask(row.id, source="local")}
                 >Remove</button>
               </li>))}
           </ul>
